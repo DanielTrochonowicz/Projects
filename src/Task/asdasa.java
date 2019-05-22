c3a07e6e70880c7717d242774552d476f08b7983
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-05-22 18:43:44 +0200

    Task update

---
+++ src/Task/asdasa.java
@@ -0,0 +1,31 @@
+package Task;
+import Task.Generic.Map;
+
+public class asdasa {
+    public static void main(String[] args) {
+        Map<String, Integer> mapa = new Map<>();
+
+        mapa.
+
+    }
+}
+
+class Slowniki{
+
+    public String wartosc;
+    public int klucz ;
+
+    public void put(String wartosc, int klucz){
+
+    }
+    public String get(int klucz){
+
+        return wartosc;
+    }
+    public int remove(){
+
+        int klucz;
+
+        return remove();
+    }
+}
