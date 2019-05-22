1db999f552a3575997d9bbda2ed9a44149c281b7
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-05-22 20:11:46 +0200

    Task update

---
+++ src/Task/_11_TypyGeneryczneMap.java
@@ -0,0 +1,42 @@
+package Task;
+
+import java.util.HashMap;
+
+public class _11_TypyGeneryczneMap {
+    public static void main(String[] args) {
+
+        Map<String, Integer> mapa = new Map<>();
+
+        mapa.put("Alicja",1);
+        mapa.put("Bartosz",2);
+        mapa.put("Celina",3);
+        mapa.put("Dawid",4);
+        mapa.put("Emilka",5);
+        mapa.put("Filip",6);
+        mapa.put("Grzegorz",7);
+        mapa.put("Halina",8);
+
+        System.out.println(mapa);
+        System.out.println(mapa.remove("Bartosz"));
+        System.out.println(mapa);
+    }
+}
+class Map<K, T>{
+    private HashMap<K,T> mapa = new HashMap<>();
+
+    public void put (K key, T value){
+        this.mapa.put(key,value);
+    }
+    public T get(K key){
+        return this.mapa.get(key);
+    }
+    public T remove(K key){
+        return  this.mapa.remove(key);
+    }
+    @Override
+    public String toString() {
+        return "Map: " +
+                "Mapa: " + mapa;
+    }
+}
+
