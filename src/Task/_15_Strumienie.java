1a778ff5e8d63b5891988e7592a5f22001e46879
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-06-12 21:03:55 +0200

    Task update

--- src/Task/_15_Strumienie.java
+++ src/Task/_15_Strumienie.java
@@ -6,22 +6,15 @@ import java.util.stream.Collectors;
 
 public class _15_Strumienie {
 
-    private List<String> ;
-
     public static void main(String[] args) {
 
         List<String> stringList = Arrays.asList("abc", "abcdefg",
                 "def", "idziemy", "abcdefg", "abdcc");
 
-        System.out.println(task1(stringList));
-
-
+        task1(stringList);
     }
     public static void task1(List<String> stringList) {
-//        return stringList.stream()
-//                .filter(s -> s.length() > 3)
-//                .collect(Collectors.toList());
-         System.out.println(task1(stringList.stream().filter(s -> s.length() > 3)
+        System.out.println(task1(stringList.stream().filter(s -> s.length() > 3)
                 .collect(Collectors.toList())));
     }
 }
