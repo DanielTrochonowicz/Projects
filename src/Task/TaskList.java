4a6a121f0288ce0b7de3358843d54a8efbf99058
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-10-29 12:36:39 +0100

    Builder Pattern

--- src/Task/TaskList.java
+++ src/Task/TaskList.java
@@ -1,8 +1,13 @@
 package Task;
 
+import java.util.LinkedList;
+
 public class TaskList {
     public static void main(String[] args) {
 
+
+        LinkedList linkedList = new LinkedList();
+//        linkedList.add();
 
     }
 }
