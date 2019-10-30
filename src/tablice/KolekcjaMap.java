9028fd08a2eb09f4489bfdba38de7f3d36da25db
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-10-30 14:52:26 +0100

    Mapy task

--- src/tablice/KolekcjaMap.java
+++ src/tablice/KolekcjaMap.java
@@ -8,6 +8,30 @@ public class KolekcjaMap {
 
         Map<Integer, String> mapa = new HashMap<>();
 
+        mapa.put(1, "jeden");
+        mapa.put(2, "dwa");
+        mapa.put(3, "trzy");
+        mapa.put(4, "cztery");
+        mapa.put(5, "piec");
 
+
+        System.out.println("Czy zawiera wartosc: " + mapa.containsValue("trzy"));
+        System.out.println("Czy zawiera klucz: " + mapa.containsKey(6));
+
+        for (Map.Entry<Integer, String> m : mapa.entrySet()){
+            int klucz = m.getKey();
+            String wartosc = m.getValue();
+
+            System.out.println("Klucz: " + klucz + " " + "wartosc: " + wartosc);
+        }
+
+        System.out.println("Wartosc spod konkretnego klucza: " + mapa.get(2));
+        System.out.println("czy zawiera wartosci: " + mapa.values());
+
+        System.out.println("HashCode: " + mapa.hashCode());
+
+        mapa.clear();
+
+        System.out.println("czy zawiera wartosci: " + mapa.values());
     }
 }
