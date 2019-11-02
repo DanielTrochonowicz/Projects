256c3ec4e844ee3bbe0ab865e529a4bfe3d04558
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-02 18:10:03 +0100

    pliki

--- src/pliki/Pliki.java
+++ src/pliki/Pliki.java
@@ -1,10 +1,19 @@
 package pliki;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.Buffer;
 
 public class Pliki {
     public static void main(String[] args) throws IOException {
+
+        //odczyt i zapis moga powodowac wyjatki dlatego nalezy je obsługiwac
+        // nelzy obslugiwac wejscia i wyjscia jak zapisujemy do pliku czy odczutujemy
+        // wlasnie po przez wyjatki
+        //zeby np nie nadpisywal sie nam juz istniejacy plik
+
 
         String fileName = "plik.txt";
         FileWriter fileWriter = null;
@@ -25,9 +34,32 @@ public class Pliki {
         }finally {
             if (fileWriter == null){
                 System.out.println("Problem");
+                fileWriter.close();
             }else{
                 fileWriter.close();
             }
+        }
+
+
+        BufferedReader bufferedReader = null;
+        try{
+            bufferedReader = new BufferedReader(new FileReader(fileName));
+            String linia = null;
+            do{
+                linia = bufferedReader.readLine();
+                if (linia != null)
+                System.out.println(linia);
+            }while (linia != null);
+        }catch (IOException ex){
+            System.out.println("Problem z dostepem do pliku");
+        }finally {
+            bufferedReader.close();
+//            if (bufferedReader != null){
+//                System.out.println("Problem");
+//                bufferedReader.close();
+//            }else {
+//                bufferedReader.close();
+//            }
         }
     }
 }
