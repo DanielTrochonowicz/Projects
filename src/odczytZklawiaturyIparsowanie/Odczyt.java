99c89604dff58ee9267e2e01f5a6c69188c18a80
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-02 16:20:31 +0100

    odczyt i parsowanie

--- src/odczytZklawiaturyIparsowanie/Odczyt.java
+++ src/odczytZklawiaturyIparsowanie/Odczyt.java
@@ -9,11 +9,50 @@ public class Odczyt {
 
         String str;
 
-        System.out.println("Wpisz cos: ");
+        System.out.println("Wpisz coś: ");
 
         str = scanner.nextLine();
 
         System.out.println("Wpisałeś: " + str);
+
+//        try {
+//            int x = Integer.parseInt(str);
+//            System.out.println(x);
+//        }catch (Exception e){
+//            System.out.println("niepoprawna liczba");
+//            main(null);
+//        }
+//
+//        try {
+//            System.out.println("Podaj liczbę: ");
+//            int liczba = scanner.nextInt();
+//            String str2 = String.valueOf(liczba);
+//            System.out.println(str2);
+//        }catch (Exception e){
+//            System.out.println("Podaj liczbe całkowita");
+//            main(null);
+//        }
+//
+//        try {
+//            System.out.println("Podaj liczbę: ");
+//            String liczba1 = scanner.nextLine();
+//            float str3 = Float.valueOf(liczba1);
+//            System.out.println(str3);
+//        }catch (Exception e){
+//            System.out.println("Podaj liczbe float");
+//            main(null);
+//        }
+
+        try {
+            System.out.println("Podaj liczbę: ");
+            String liczba2 = scanner.nextLine();
+            double str4 = Double.valueOf(liczba2);
+            String str5 = String.valueOf(str4);
+            System.out.println(str4);
+        }catch (Exception e){
+            System.out.println("Podaj liczbe double");
+            main(null);
+        }
 
 
     }
