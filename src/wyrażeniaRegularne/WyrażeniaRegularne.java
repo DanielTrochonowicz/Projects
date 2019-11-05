954c0b63b8e4df5fafed1e9bfd0c7c488dc81865
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-05 15:49:09 +0100

    Wyrażenia regularne

--- src/wyrażeniaRegularne/WyrażeniaRegularne.java
+++ src/wyrażeniaRegularne/WyrażeniaRegularne.java
@@ -6,9 +6,27 @@ import java.util.regex.Pattern;
 public class WyrażeniaRegularne {
     public static void main(String[] args) {
 
-        Pattern p = Pattern.compile("Java");
-        Matcher m = p.matcher("Java");
+        // ^ początek wzorca
+        // $ koniec wzorca
+        // [0-9] \\d        wszystkie znaki od 0 do 9
+        // [^0-9]  \\D     ten znak ^ oznacza negacje znaki od 0-9 nie moga wystapic
+        //[a-zA-Z] \\w
+        //[^a-zA-Z] \\W
+        // \\s = dowolny znak biały
+        // \\S = wszystko tylko nie spacja
+        // \\t = moze byc tabulator
+        // \\n znak nowej lini
 
+        //  [a]{x} = x wystapieni litery a
+        //  [a]{1, } = [a]+ jeden lub wiecej razy
+        //  [a]{1,3} = litera a wystepuje min 1 raz / max 3
+        //  [a]{0,} = [a]* moze sie pojawic ale nie musi
+        //  [a]? = [a]{0,1} = nie wiecej niz raz
+
+        Pattern p = Pattern.compile("Java");
+        Matcher m = p.matcher("Programowanie Java");
+
+        System.out.println(m.find());
         System.out.println(m.matches());
     }
 }
