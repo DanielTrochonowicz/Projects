21d83b74b7966a8fa28931d093bbba24d7dde098
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-05 14:50:15 +0100

    siecCzat

--- src/siecCzatServer/Klient.java
+++ src/siecCzatServer/Klient.java
@@ -1,6 +1,8 @@
 package siecCzatServer;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -26,6 +28,25 @@ public class Klient {
         try {
             Socket socket = new Socket(IP, PORT);
             System.out.println("Podłączono do " + socket);
+
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+
+            while (true){
+                System.out.println(">> ");
+                String srt = scanner.nextLine();
+                if (srt.equalsIgnoreCase("q")){
+                    printWriter.println(imie + ": " + srt);
+                    printWriter.flush();
+                }else {
+                    printWriter.println(imie + " rozłączył się");
+                    printWriter.flush();
+                    printWriter.close();
+                    scanner.close();
+                    socket.close();
+                }
+            }
         }catch (Exception ex){
 
         }
