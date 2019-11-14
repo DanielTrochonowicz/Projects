bad6831722399108078f471977feb153cef41f9a
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-14 16:57:52 +0100

    task

---
+++ src/task2/binaryGap/BinaryGap.java
@@ -0,0 +1,42 @@
+package task2.binaryGap;
+
+import task2.countDiv.Solution;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BinaryGap {
+
+    public int solution(int n) {
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        String binaryNumber = Integer.toBinaryString(n);
+        int biggestGap = 0;
+        int temp = 0;
+        Integer value;
+        List<Integer> integerList = Arrays.stream(binaryNumber.split("")).map(Integer::parseInt).collect(Collectors.toList());
+
+
+        if (integerList.stream().filter(c -> c == 1).count() == 1) {
+            return 0;
+        } else {
+
+            for (int i = 0; i < integerList.size(); i++) {
+                value = integerList.get(i);
+                {
+                    if (value == 0) {
+                        temp++;
+                        if (temp >= biggestGap) {
+                            biggestGap = temp;
+                        }
+                    } else if (value == 1) {
+                        temp = 0;
+                    }
+                }
+            }
+        }
+        return biggestGap;
+    }
+}
