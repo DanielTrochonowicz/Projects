e25fe30965814124c584bd6d8a1466c756e8edf8
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-21 16:01:48 +0100

    task

--- src/task2/absDistinct/AbsDistinct.java
+++ src/task2/absDistinct/AbsDistinct.java
@@ -4,6 +4,21 @@ import java.util.Arrays;
 
 public class AbsDistinct {
 
+    public static void main(String[] args) {
+
+        int[] a = new int[10];
+        a[0] = -5;
+        a[1] = -3;
+        a[2] = -1;
+        a[3] =  0;
+        a[4] =  3;
+        a[5] =  6;
+        a[6] =  8;
+        a[7] =  10;
+        a[8] =  11;
+
+    }
+
     public static int absDistinct(int[] a) {
         if(a.length == 1){
             return 1;
