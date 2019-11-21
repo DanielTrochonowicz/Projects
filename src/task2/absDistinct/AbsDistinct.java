26776a015a001f1791e02a6d8d3a2510476b8720
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-21 16:01:23 +0100

    task

--- src/task2/absDistinct/AbsDistinct.java
+++ src/task2/absDistinct/AbsDistinct.java
@@ -1,5 +1,22 @@
 package task2.absDistinct;
 
+import java.util.Arrays;
+
 public class AbsDistinct {
 
+    public static int absDistinct(int[] a) {
+        if(a.length == 1){
+            return 1;
+        }
+        for (int i = 0; i < a.length; i ++){
+            if (a[i] <0)
+                a[i] *= -1;
+        }
+        int distinctElements = (int) Arrays.stream(a).distinct().count();
+
+        if (a.length == distinctElements) {
+            return a.length;
+        }
+        return (int) Arrays.stream(a).distinct().count();
+    }
 }
