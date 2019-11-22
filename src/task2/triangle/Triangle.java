4cb678170b9208feeaf1cc270f387c9abfffb4c2
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-22 13:43:24 +0100

    task

--- src/task2/triangle/Triangle.java
+++ src/task2/triangle/Triangle.java
@@ -1,12 +1,18 @@
 package task2.triangle;
 
-public class Triangle {
+import java.util.Arrays;
 
+public class Triangle {
 
     public int triangle(int[] a) {
         if (a.length <= 2) {
             return 0;
         }
-
+        Arrays.sort(a);
+        for (int i = 0; i < a.length - 2; i++) {
+            if (a[i] > a[i + 2] - a[i + 1]) {
+                return 1;
+            }
+        }return 0;
     }
 }
