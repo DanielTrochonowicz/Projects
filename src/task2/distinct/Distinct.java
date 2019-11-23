1f5c69f70d53d69d4c18aeea97d0586a693f98c6
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-23 14:44:27 +0100

    task

--- src/task2/distinct/Distinct.java
+++ src/task2/distinct/Distinct.java
@@ -1,4 +1,15 @@
 package task2.distinct;
 
+import java.util.Arrays;
+
 public class Distinct {
+
+    public int distinct(int[] A) {
+        if (A.length == 0)
+            return 0;
+        return (int) Arrays.stream(A)
+                .distinct()
+                .boxed()
+                .count();
+    }
 }
