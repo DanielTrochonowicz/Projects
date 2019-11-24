37a81c68fa866870bc77b1e8d3114fa19c9aa3b0
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-24 16:15:03 +0100

    task

--- src/task2/frogJump/FrogJump.java
+++ src/task2/frogJump/FrogJump.java
@@ -1,4 +1,11 @@
 package task2.frogJump;
 
 public class FrogJump {
+
+    public int frogJump(int X, int Y, int D) {
+        if ((Y-X)%D==0){
+            return (Y-X)/D;
+        }
+        return (Y-X)/D+1;
+    }
 }
