46b357997a21d1ee59374255e31756cfd2fac55c
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-25 08:13:45 +0100

    task

--- src/task2/arrayInversionCount/ArrayInversionCount.java
+++ src/task2/arrayInversionCount/ArrayInversionCount.java
@@ -1,4 +1,29 @@
 package task2.arrayInversionCount;
 
+import java.util.Arrays;
+
 public class ArrayInversionCount {
+
+    public int arrayInversionCount(int[] A) {
+
+        int distinctElements = (int) Arrays.stream(A).distinct().count();
+
+        if (A.length <= 1 || (A.length == 2 && A[0] >= A[1]) || distinctElements == 1)
+            return 0;
+
+        int counter = 0;
+        for (int i = A.length - 1; i > 0; i--) {
+            for (int j = A.length - 2; j >= 0; j--) {
+                if (i > j) {
+                    if (A[i] < A[j]) {
+                        counter++;
+                    }
+                }
+            }
+        }
+        if (counter <= 1_000_000_000)
+            return counter;
+        return -1;
+    }
+
 }
