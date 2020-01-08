2f436038510a729c67b4cc1bf31bd0ce1d85e998
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2020-01-08 01:06:51 +0100

    random + add tab ?

--- src/task/RandomTab.java
+++ src/task/RandomTab.java
@@ -1,7 +1,4 @@
 package task;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 
 public class RandomTab {
@@ -12,6 +9,5 @@ public class RandomTab {
         int tab = random.nextInt(50);
 
         System.out.println(tab);
-
     }
 }
